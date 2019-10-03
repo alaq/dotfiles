6e2ae986f5502c18cc50c2c1798ea44e6029fde0
Adrien Lacquemant <github@alaq.io>
2019-10-03 18:02:04 -0400

    Add org-capture templates

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -9,17 +9,33 @@
 (blink-cursor-mode t)
 
 (setq org-agenda-files (quote ("~/org/tasks.org" "~/org/journal.org")))
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/org/tasks.org" "Next")
-          "* THEN %?\n %U")
-        ("j" "Journal" entry (file+datetree "~/org/journal.org")
-          "* %?\n %T")
-        ("d" "DONE" entry (file+datetree "~/org/journal.org")
-          "* DONE %?\n %T")))
+
+(after! org
+  (add-to-list 'org-capture-templates
+               '("td" "Todo" entry
+                 (file+headline "~/org/tasks.org")
+                 "* THEN %?\n %U"
+                 :empty-lines 1))
+  (add-to-list 'org-capture-templates
+               '("wt" "Work item" entry
+                 (file+headline "~/org/tasks.org" "Next")
+                 "* THEN %?\n %U"
+                 :empty-lines 1))
+  (add-to-list 'org-capture-templates
+               '("j" "Journal" entry
+                 (file+datetree "~/org/journal.org")
+                 "* %?\n %T"
+                 :empty-lines 1))
+  (add-to-list 'org-capture-templates
+               '("d" "DONE" entry
+                 (file+datetree "~/org/journal.org")
+                 "* DONE %?\n %T"
+                 :empty-lines 1)))
 
 (setq org-bullets-bullet-list (quote ("◉" "○")))
 
 (setq display-line-numbers-type 'relative)
+
 
 (projectile-add-known-project "/mnt/c/cats")
 (projectile-add-known-project "~/org")
