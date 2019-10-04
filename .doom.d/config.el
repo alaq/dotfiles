6fd988d3c111c61c62c79168a023fd8b98c4acf2
Adrien Lacquemant <github@alaq.io>
2019-10-04 17:38:25 -0400

    Add org-capture popup to i3

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -37,5 +37,54 @@
 (setq display-line-numbers-type 'relative)
 
 
-(projectile-add-known-project "/mnt/c/cats")
-(projectile-add-known-project "~/org")
+
+
+;; External frame from ./module...
+
+;;;###autoload
+(defvar +org-capture-frame-parameters-temp
+  `((name . "org-capture")
+    (width . 70)
+    (height . 50)
+    (transient . t)
+    ,(if IS-LINUX '(display . "172.25.112.1:0")))
+  "TODO")
+
+;;;###autoload
+(defun +org-capture/open-frame-temp (&optional initial-input key)
+  "Opens the org-capture window in a floating frame that cleans itself up once
+you're done. This can be called from an external shell script."
+  (interactive)
+  (when (and initial-input (string-empty-p initial-input))
+    (setq initial-input nil))
+  (when (and key (string-empty-p key))
+    (setq key nil))
+  (let* ((frame-title-format "")
+         (frame (if (+org-capture-frame-p)
+                    (selected-frame)
+                  (make-frame +org-capture-frame-parameters-temp))))
+    (select-frame-set-input-focus frame)  ; fix MacOS not focusing new frames
+    (with-selected-frame frame
+      (require 'org-capture)
+      (condition-case ex
+          (cl-letf (((symbol-function #'pop-to-buffer)
+                     (symbol-function #'switch-to-buffer)))
+            (switch-to-buffer (doom-fallback-buffer))
+            (let ((org-capture-initial initial-input)
+                  org-capture-entry)
+              (when (and key (not (string-empty-p key)))
+                (setq org-capture-entry (org-capture-select-template key)))
+              (if (or org-capture-entry
+                      (not (fboundp 'counsel-org-capture)))
+                  (org-capture)
+                (unwind-protect
+                    (counsel-org-capture)
+                  (if-let* ((buf (cl-loop for buf in (buffer-list)
+                                          if (buffer-local-value 'org-capture-mode buf)
+                                          return buf)))
+                      (with-current-buffer buf
+                        (add-hook 'kill-buffer-hook #'+org-capture-cleanup-frame-h nil t))
+                    (delete-frame frame))))))
+        ('error
+         (message "org-capture: %s" (error-message-string ex))
+         (delete-frame frame))))))
