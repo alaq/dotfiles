4087203cfbd108253f9c3bc4e14f2ffe784bd713
Adrien Lacquemant <github@alaq.io>
2019-10-09 10:17:37 -0400

    Add clone or pull logic to git repos in bootstrap and make the stdout/stderr silent

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
 # Add ppa
-sudo add-apt-repository ppa:kelleyk/emacs -y &&
-sudo add-apt-repository ppa:kgilmer/speed-ricer -y &&
+sudo add-apt-repository ppa:kelleyk/emacs -y >/dev/null 2>&1 &&
+sudo add-apt-repository ppa:kgilmer/speed-ricer -y >/dev/null 2>&1 &&
 
-sudo apt update &&
-sudo apt upgrade &&
-sudo apt install git zsh emacs26 firefox i3-gaps polybar fonts-font-awesome -y &&
+sudo apt update >/dev/null 2>&1 &&
+sudo apt upgrade -y >/dev/null 2>&1 &&
+sudo apt install git zsh emacs26 firefox i3-gaps polybar fonts-font-awesome -y >/dev/null 2>&1 &&
 
 is_wsl=$(cat /proc/version | grep "microsoft" | wc -l)
 if [ ! $is_wsl -eq 1 ]
 then
     echo "Not using WSL, installing i3-gnome."
-    git clone https://github.com/i3-gnome/i3-gnome.git &&
-    cd i3-gnome &&
-    sudo make install
+    sudo apt install gnome-flashback -y >/dev/null 2>&1 &&
+    git clone https://github.com/i3-gnome/i3-gnome.git || git -C i3-gnome pull >/dev/null 2>&1 &&
+    cd i3-gnome >/dev/null 2>&1 &&
+    sudo make install >/dev/null 2>&1
 fi
 
-chsh -s /bin/zsh && # change shell to zsh
+sudo chsh -s /bin/zsh >/dev/null 2>&1 && # change shell to zsh
 
 # emacs
-rm -r ~/.emacs.d &&
-git clone https://github.com/hlissner/doom-emacs -b develop ~/.emacs.d &&
-cd ~/.emacs.d &&
-./.emacs.d/bin/doom refresh &&
+git clone https://github.com/hlissner/doom-emacs -b develop ~/.emacs.d || git -C ~/.emacs.d pull >/dev/null 2>&1 &&
+./.emacs.d/bin/doom refresh >/dev/null 2>&1
 
 # vscode
-sudo snap install --classic code
+sudo snap install --classic code >/dev/null 2>&1
 if command -v code >/dev/null; then
     # from `code --list-extensions`
     modules="
@@ -41,6 +40,6 @@ vscodevim.vim
     done
 fi
 
-git clone --bare https://github.com/alaq/dotfiles.git $HOME/.dotfiles
+git clone --bare https://github.com/alaq/dotfiles.git $HOME/.dotfiles || git -C ~/.dotfiles pull >/dev/null 2>&1
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 config checkout
