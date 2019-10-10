53bc961b388fd704bae577840f36aae24332568c
Adrien Lacquemant <github@alaq.io>
2019-10-10 11:20:31 -0400

    Add logs to monitor progress in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 # Add ppa
+echo "Adding PPAs"
 sudo add-apt-repository ppa:kelleyk/emacs -y >/dev/null 2>&1 &&
 sudo add-apt-repository ppa:kgilmer/speed-ricer -y >/dev/null 2>&1 &&
 
+echo "Updating Ubuntu's packages"
 sudo apt update >/dev/null 2>&1 &&
 sudo apt upgrade -y >/dev/null 2>&1 &&
+echo "Installing software"
 sudo apt install git zsh emacs26 firefox i3-gaps polybar fonts-font-awesome -y >/dev/null 2>&1 &&
 
 is_wsl=$(cat /proc/version | grep "microsoft" | wc -l)
@@ -18,13 +21,16 @@ then
     sudo make install >/dev/null 2>&1
 fi
 
+echo "Changing default shell to zsh"
 sudo chsh -s /bin/zsh >/dev/null 2>&1 && # change shell to zsh
 
 # emacs
+echo "Installing/updating doom-emacs"
 git clone https://github.com/hlissner/doom-emacs -b develop ~/.emacs.d || git -C ~/.emacs.d pull >/dev/null 2>&1 &&
 ./.emacs.d/bin/doom refresh >/dev/null 2>&1
 
 # vscode
+echo "Installing vscode and extensions"
 sudo snap install --classic code >/dev/null 2>&1
 if command -v code >/dev/null; then
     # from `code --list-extensions`
@@ -40,6 +46,7 @@ vscodevim.vim
     done
 fi
 
+echo "Getting dotfiles"
 git clone --bare https://github.com/alaq/dotfiles.git $HOME/.dotfiles || git -C ~/.dotfiles pull >/dev/null 2>&1
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 config checkout
