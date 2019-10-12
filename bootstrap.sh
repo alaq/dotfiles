11ad037f0c42ba88b085209d3e128c8d83ac3e0c
Adrien Lacquemant <github@alaq.io>
2019-10-12 19:17:22 -0400

    Log messages in bold

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,43 @@
 #!/usr/bin/env bash
 set -e
 
+bold () {
+    echo -e "\033[1m$1\033[0m"
+}
+
 # Add ppa
-echo "Adding PPAs"
+bold "Adding PPAs"
 # sudo add-apt-repository ppa:kelleyk/emacs -y
 # sudo add-apt-repository ppa:kgilmer/speed-ricer -y
 
-echo "Updating Ubuntu's packages"
+bold "Updating Ubuntu's packages"
 sudo apt update
 sudo apt upgrade -y
-echo "Installing software"
+bold "Installing software"
 sudo apt install git zsh emacs26 rxvt-unicode i3-wm dmenu i3status fonts-font-awesome arc-theme -y
 # i3-gaps polybar
 
 is_wsl=$(cat /proc/version | grep "microsoft" | wc -l)
 if [ ! $is_wsl -eq 1 ]
 then
-    echo "Not using WSL, installing i3-gnome."
+    bold "Not using WSL, installing i3-gnome."
     sudo apt install gnome-flashback -y
     git clone https://github.com/i3-gnome/i3-gnome.git 2> /dev/null || git -C i3-gnome pull
     cd i3-gnome
-    sudo make install >/dev/null 2>&1
+   sudo make install
 fi
 
-echo "Changing default shell to zsh"
+bold "Changing default shell to zsh"
 sudo chsh -s /bin/zsh # change shell to zsh
 
 # emacs
-echo "Installing/updating doom-emacs"
+bold "Installing/updating doom-emacs"
 git clone https://github.com/hlissner/doom-emacs -b develop ~/.emacs.d 2> /dev/null || git -C ~/.emacs.d pull
 cd
 ./.emacs.d/bin/doom refresh
 
 # vscode
-echo "Installing vscode and extensions"
+bold "Installing vscode and extensions"
 sudo snap install --classic code
 if command -v code >/dev/null; then
     # from `code --list-extensions`
@@ -49,7 +53,7 @@ vscodevim.vim
     done
 fi
 
-echo "Getting dotfiles"
+bold "Getting dotfiles"
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone --bare https://github.com/alaq/dotfiles.git $HOME/.dotfiles 2> /dev/null || /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME pull
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
